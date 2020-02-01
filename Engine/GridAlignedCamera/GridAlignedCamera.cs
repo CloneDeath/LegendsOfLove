c860fb8552ef553be961d4ab42fc0e9441c1ba65
Nicholas Rodine <halfofastaple@gmail.com>
2020-01-31 23:55:09 -0500

    Interpolated player as well, and handled case when player moved into a corner.

--- Engine/GridAlignedCamera/GridAlignedCamera.cs
+++ Engine/GridAlignedCamera/GridAlignedCamera.cs
@@ -1,27 +1,50 @@
+using System.Collections.Generic;
+using System.Linq;
 using Godot;
 using LegendsOfLove.Entities.Player;
 
 namespace LegendsOfLove.Engine.GridAlignedCamera {
-    public partial class GridAlignedCamera : Node2D
-    {
+    public partial class GridAlignedCamera : Node2D {
+        protected Queue<TransitionAction> TransitionQueue = new Queue<TransitionAction>();
+        
         public override void _PhysicsProcess(float delta) {
             Camera2D.GlobalPosition = GlobalPosition.Round();
+
+            CheckForTransitions();
+        }
+
+        protected void CheckForTransitions() {
+            if (!CanTransition) return;
+            if (!TransitionQueue.Any()) return;
+
+            var action = TransitionQueue.Dequeue();
+            Transition(action.Direction, action.Player);
         }
 
         public void _on_Player_body_entered(Node body, Vector2 direction) {
             if (!(body is Player player)) return;
-            if (Tween.IsActive()) return;
+            if (!CanTransition) {
+                TransitionQueue.Enqueue(new TransitionAction(player, direction));
+                GD.Print("corner case");
+                return;
+            }
 
-            Transition(direction);
-            player.Position += direction * 12;
+            Transition(direction, player);
         }
 
-        protected void Transition(Vector2 direction) {
+        protected bool CanTransition => !Tween.IsActive();
+
+        protected void Transition(Vector2 direction, Player player) {
             Tween.RemoveAll();
 
             var delta = direction * new Vector2(72, 48);
             Tween.InterpolateProperty(this, nameof(Position),
                 Position, Position + delta, 1);
+
+            var playerDelta = direction * new Vector2(6, 6);
+            Tween.InterpolateProperty(player, nameof(player.Position),
+                player.Position, player.Position + playerDelta, 1);
+
             Tween.Start();
         }
     }
