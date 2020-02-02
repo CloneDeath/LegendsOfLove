1cdb5c5c7ff8fd3aea50ae58709a47aa87c395c3
Nicholas Rodine <halfofastaple@gmail.com>
2020-02-01 20:18:11 -0500

    Added Title Screen. Made the Dither rect more generic.

---
+++ Title/MainMenu.cs
@@ -0,0 +1,19 @@
+using Godot;
+
+namespace LegendsOfLove.Title {
+    public partial class MainMenu : Control {
+        private bool _triggered = false;
+        
+        public void StartGame() {
+            GetTree().ChangeScene("res://Main.tscn");
+        }
+
+        public override void _Input(InputEvent @event) {
+            if (_triggered) return;
+            if (!@event.IsPressed()) return;
+
+            _triggered = true;
+            AnimationPlayer.Play("GameStart");
+        }
+    }
+}
