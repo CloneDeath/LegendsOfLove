d1b1ed4aa3780dce27483dc1dae59eeb8c2f9c83
Nicholas Rodine <halfofastaple@gmail.com>
2020-02-04 15:43:40 -0500

    Fixed locked block not making a sound when opened

--- Entities/LockedBlock/LockedBlock.cs
+++ Entities/LockedBlock/LockedBlock.cs
@@ -6,9 +6,16 @@ namespace LegendsOfLove.Entities.LockedBlock {
 	{
 		public Player.Player GetPlayer() => GetTree().GetNodesInGroup("player").Cast<Player.Player>().FirstOrDefault();
 
+		protected AudioStreamPlayer OpenSound => GetNode<AudioStreamPlayer>("OpenSound");
+
 		public void Push(Vector2 direction) {
 			var player = GetPlayer();
 			if (player == null || !player.HasKey1) return;
+
+			var sound = OpenSound;
+			RemoveChild(sound);
+			GetPlayer().AddChild(sound);
+			sound.Play();
 
 			QueueFree();
 		}
